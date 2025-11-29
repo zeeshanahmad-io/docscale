17e1cf4055ff12f98e16eb10e117e727566c89d1
Zeeshan Ahmad <zeeshan.official@gmail.com>
2025-11-29 16:57:27 +0530

    removed extra padding

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -229,7 +229,7 @@ const BlogPost = () => {
             )}
 
             {/* Main Content */}
-            <div className="flex-1 max-w-4xl min-w-0 px-2 md:px-0">
+            <div className="flex-1 max-w-4xl min-w-0">
               {/* Article Header without Placeholder Image */}
               <header className="mb-16 text-center">
                 {post.featuredImage && (
