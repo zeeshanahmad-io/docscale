fd7e610e7c6cbde936bc97a6a4458012b8e4adb9
Zeeshan Ahmad <zeeshan.official@gmail.com>
2025-11-29 15:59:18 +0530

    added a bit of padding for mobile

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -305,7 +305,7 @@ const BlogPost = () => {
               )}
 
               {/* Enhanced Article Content */}
-              <div className="blog-content">
+              <div className="blog-content pr-2 md:pr-0">
                 <ReactMarkdown
                   components={{
                     h1: ({ children, ...props }) => (
